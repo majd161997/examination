e9d5e5efce32eb1b4365a4ef6da8ea5e728691f6
salam1107 <48412374+salam1107@users.noreply.github.com>
2019-09-12 17:47:04 +0300

    Update update_email.php

--- AJAX/update_email.php
+++ AJAX/update_email.php
@@ -2,6 +2,7 @@
     session_start();
     require_once('../secure/config.php');
     $id = $_SESSION["id"];
+
     $email = mysqli_real_escape_string($connect, $_POST['email']);
     $update_email = mysqli_query($connect, "UPDATE accounts SET Email='$email' WHERE id='$id'");
 ?>
